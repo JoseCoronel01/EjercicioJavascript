1b766d1cde4285d0390600d7d36f6968fa0398b3
ProyectosNET <josecoronel88@gmail.com>
2022-04-13 19:19:57 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioJavascript/Inicio.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="EjercicioJavascript.Inicio" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Prueba</title>
+    <script src="Scripts/jquery-3.6.0.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input type="text" name="Fecha" id="Fecha" placeholder="dd/mm/yyyy" />
+            <input type="text" name="Comentarios" id="Comentarios" placeholder="Comentarios" />
+            <script type="text/javascript">
+                var idCliente = 2; //id cliente creado en el ejercicio 3 de mysql.
+
+                function saveGestion() {
+                    var fecha = $('#Fecha').val();
+                    var comentarios = $('#Comentarios').val();
+                    $.ajax({
+                        async: false,
+                        url: '/Inicio.aspx/saveGestion',
+                        type: 'JSON',
+                        contentType: 'application/json; charset=utf-8',
+                        data: '{"Fecha": "'+fecha+'", "IdCliente": '+idCliente+', "Comentarios": "'+comentarios+'"}',
+                        success: function (data) {
+                            if (data.d.Exito) {
+                                alert('Gestion creada con éxito. ' + data.d.Mensaje);
+                            } else {
+                                alert('Hubo un error en el servidor. ' + data.d.Mensaje);
+                            }
+                        },
+                        error: function (error) {
+                            alert(error);
+                        }
+                    });
+                }
+
+                function UC_exec(sql) {
+                    if (mysql.exec(sql)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            </script>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EjercicioJavascript/Inicio.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace EjercicioJavascript
+{
+    public partial class Inicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static Respuesta saveGestion(string Fecha, int IdCliente, string Comentarios)
+        {
+            Respuesta res = new Respuesta();
+            try
+            {
+                res.Exito = false;
+
+                using (var mysql = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=clientes;User Id=root;Password=123456;"))
+                {
+                    mysql.Open();
+
+                    using (var cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = mysql;
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.CommandText = "INSERT INTO gestiones (idcliente,fecha,res1,comentarios) " +
+                            "values (@idcliente,@fecha,@res1,@comentarios)";
+                        cmd.Parameters.AddWithValue("@idcliente", IdCliente);
+                        cmd.Parameters.AddWithValue("@Fecha", DateTime.Parse(Fecha));
+                        cmd.Parameters.AddWithValue("@res1", "Pago agendado");
+                        cmd.Parameters.AddWithValue("@comentarios", Comentarios);
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read() && reader != null)
+                        {
+                            res.Exito = true;
+                            reader.Close();
+                            //hasta aqui el ejercicio 1 de javascript.
+
+                            //Ejercicio 2 de javascript.
+                            if (Comentarios == "Eliminar Cliente")
+                            {
+                                using (var cmd2 = new MySqlCommand())
+                                {
+                                    cmd2.Connection = mysql;
+                                    cmd2.CommandType = System.Data.CommandType.Text;
+                                    cmd2.CommandText = "Delete from clientes where id = @id";
+                                    cmd2.Parameters.AddWithValue("@id", IdCliente);
+                                    MySqlDataReader reader2 = cmd2.ExecuteReader();
+                                    if (reader2.Read() && reader2 != null)
+                                    {
+                                        res.Mensaje = "Se elimino al cliente.";
+                                    }
+                                    reader2.Close();
+                                }
+                            }
+                            
+                        }
+                    }
+
+                    mysql.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                res.Exito = false;
+                res.Mensaje = ex.Message.ToString();
+            }
+            finally { }
+            return res;
+        }
+    }
+
+    public class Respuesta
+    {
+        public bool Exito { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
+
